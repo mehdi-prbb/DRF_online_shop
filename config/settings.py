a43efe976370816b88e9c7ea7e04f0d21b1465a7
mehdi pourbaba <mpourbaba373@gmail.com>
2023-11-11 21:23:20 +0330

    set database settnigs on mysql

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'drf_online_shop',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'm1371521p',
     }
 }
 
