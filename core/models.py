0bc25780144fef78ab8217a5ee8f5adb878a2aaf
mehdi pourbaba <mpourbaba373@gmail.com>
2023-11-11 22:21:10 +0330

    create CustomUser model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=11, unique=True)
+
+    USERNAME_FIELD = 'phone_number'
+
+    def __str__(self):
+        return self.phone_number
